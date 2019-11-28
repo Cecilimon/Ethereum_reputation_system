2fd0402c7988e9c7dc7280a6291eaee3f5715d38
Cecilimon <cecilimon0909@gmail.com>
2019-11-28 20:03:06 +0900

    "added Auction"

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,18 @@
 var RateStorage = artifacts.require("RateStorage");
 var Reputation = artifacts.require("Reputation");
+var Auction = artifacts.require("Auction")
 
 module.exports = function(deployer, network, accounts) {
   var manager = accounts[0];
   // var fee = 200000000000000;
-  var wait = 60;
+  var wait = 30;
 
   deployer.deploy(RateStorage)
   // Deploy etherep
   .then(function() {
-    return deployer.deploy(Reputation,RateStorage.address, wait)
+    return deployer.deploy(Reputation,RateStorage.address, wait);
+  }).then(function(){
+    return deployer.deploy(Auction);
   });
 
 
